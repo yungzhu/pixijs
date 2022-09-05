eb6b5a4be98c67a8d8b6f9822d9755e501ac8837
Zyie <24736175+Zyie@users.noreply.github.com>
2022-09-05 16:26:20 +0100

    test split

--- jest.config.js
+++ jest.config.js
@@ -1,36 +1,80 @@
 module.exports = {
-    testPathIgnorePatterns: ['/node_modules/', '/src/', '/dist/', '/lib/', '/out/'],
-    preset: 'ts-jest/presets/js-with-ts',
-    runner: '@kayahr/jest-electron-runner',
-    testEnvironment: '@kayahr/jest-electron-runner/environment',
-    setupFilesAfterEnv: [
-        'jest-extended/all',
-        // './test/debug.js',
-    ],
-    globalSetup: '<rootDir>/test/jest-global-setup.ts',
-    globalTeardown: '<rootDir>/test/jest-global-teardown.ts',
-    transform: {
-        '\\.vert$': '<rootDir>/test/jest-raw-loader.js',
-        '\\.frag$': '<rootDir>/test/jest-raw-loader.js',
-    },
-    moduleNameMapper: {
-        '^@pixi/(.*)$': '<rootDir>/packages/$1/src',
-    },
-    testMatch: ['**/?(*.)+(spec|tests).[tj]s?(x)'],
-    globals: {
-        'ts-jest': {
-            tsconfig: {
-                module: 'ESNext',
-                esModuleInterop: true,
+    projects: [
+        {
+            testPathIgnorePatterns: ['/node_modules/', '/src/', '/dist/', '/lib/', '/out/'],
+            preset: 'ts-jest/presets/js-with-ts',
+            // runner: '@kayahr/jest-electron-runner',
+            // testEnvironment: '@kayahr/jest-electron-runner/environment',
+            // runner: '@kayahr/jest-electron-runner/main',
+            testEnvironment: 'jsdom',
+            // testEnvironment: 'jsdom',
+            setupFilesAfterEnv: [
+                'jest-extended/all',
+                // './test/debug.js',
+            ],
+            globalSetup: '<rootDir>/test/jest-global-setup.ts',
+            globalTeardown: '<rootDir>/test/jest-global-teardown.ts',
+            transform: {
+                '\\.vert$': '<rootDir>/test/jest-raw-loader.js',
+                '\\.frag$': '<rootDir>/test/jest-raw-loader.js',
             },
-            diagnostics: false,
+            moduleNameMapper: {
+                '^@pixi/(.*)$': '<rootDir>/packages/$1/src',
+            },
+            testMatch: ['**/?(*.)+(spec|tests).node.[tj]s?(x)'],
+            globals: {
+                'ts-jest': {
+                    tsconfig: {
+                        module: 'ESNext',
+                        esModuleInterop: true,
+                    },
+                    diagnostics: false,
+                },
+            },
+            collectCoverageFrom: [
+                '<rootDir>/packages/**/*.ts',
+                '!<rootDir>/packages/**/*.d.ts',
+                '!<rootDir>/packages/polyfill/**/*.ts',
+            ],
+            coverageDirectory: '<rootDir>/dist/coverage',
+            testTimeout: 10000
         },
-    },
-    collectCoverageFrom: [
-        '<rootDir>/packages/**/*.ts',
-        '!<rootDir>/packages/**/*.d.ts',
-        '!<rootDir>/packages/polyfill/**/*.ts',
-    ],
-    coverageDirectory: '<rootDir>/dist/coverage',
-    testTimeout: 10000
+        {
+            testPathIgnorePatterns: ['/node_modules/', '/src/', '/dist/', '/lib/', '/out/'],
+            preset: 'ts-jest/presets/js-with-ts',
+            runner: '@kayahr/jest-electron-runner',
+            testEnvironment: '@kayahr/jest-electron-runner/environment',
+            // testEnvironment: 'jsdom',
+            setupFilesAfterEnv: [
+                'jest-extended/all',
+                // './test/debug.js',
+            ],
+            globalSetup: '<rootDir>/test/jest-global-setup.ts',
+            globalTeardown: '<rootDir>/test/jest-global-teardown.ts',
+            transform: {
+                '\\.vert$': '<rootDir>/test/jest-raw-loader.js',
+                '\\.frag$': '<rootDir>/test/jest-raw-loader.js',
+            },
+            moduleNameMapper: {
+                '^@pixi/(.*)$': '<rootDir>/packages/$1/src',
+            },
+            testMatch: ['**/?(*.)+(spec|tests).[tj]s?(x)'],
+            globals: {
+                'ts-jest': {
+                    tsconfig: {
+                        module: 'ESNext',
+                        esModuleInterop: true,
+                    },
+                    diagnostics: false,
+                },
+            },
+            collectCoverageFrom: [
+                '<rootDir>/packages/**/*.ts',
+                '!<rootDir>/packages/**/*.d.ts',
+                '!<rootDir>/packages/polyfill/**/*.ts',
+            ],
+            coverageDirectory: '<rootDir>/dist/coverage',
+            testTimeout: 10000
+        }
+    ]
 };
